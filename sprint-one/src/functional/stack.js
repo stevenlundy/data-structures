cd6efd750553ee4f8bb785f98df2dc0eb38d6548
Steven Lundy <steven.lundy@gmail.com>
2015-08-19 12:01:46 -0700

    Wrote functional implementation of Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var top = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[top++] = value;
   };
 
   someInstance.pop = function(){
+    if (top){
+      return storage[--top];
+    }
   };
 
   someInstance.size = function(){
+    return top;
   };
 
   return someInstance;
