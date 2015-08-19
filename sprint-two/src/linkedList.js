452c15acf02367e35668ca57c3bcd5ab5eedbbe0
stevenlundy <steven.lundy@gmail.com>
2015-08-19 16:20:55 -0700

    Linked list passes all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
+    else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    if (list.head === null) {
+      return;
+    }
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
   list.contains = function(target){
+      pointer = list.head;
+      while (pointer) {
+        if (pointer.value === target) {
+          return true;
+        }
+        pointer = pointer.next;
+      }
+      return false;
   };
 
   return list;
